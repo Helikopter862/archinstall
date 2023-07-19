e3df51446943c50c7b5bfccb0e6c3a3f9451cd93
Helikopter862 <130805628+Helikopter862@users.noreply.github.com>
2023-07-19 18:04:56 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $rootxfs ]
+then
+  mkfs.xfs -f $root || exit && mount $root /mnt && mkdir /mnt/boot 
+else
+  echo "Root partition not added."
+  exit
+fi
+
+if [ $rootbtrfs ]
+then
+  mkfs.btrfs -f $root || exit && mount $root /mnt && mkdir /mnt/boot 
+else
+  echo "Root partition not added."
+  exit
+fi
+
+if [ $boot ]
+then
+  mkfs.fat -F32 $boot || exit && mount $boot /mnt/boot
+else
+  echo "Boot partition not added."
+  umount /mnt
+  exit
+fi
+
+pacstrap /mnt base linux-zen linux-zen-headers linux-firmware || exit
+timedatectl set-ntp true
+genfstab -U /mnt >> /mnt/etc/fstab
+curl -L https://raw.githubusercontent.com/miguelrcborges/archinstallscript/main/chroot-script.sh -o /mnt/chroot-script.sh
+arch-chroot /mnt sh chroot-script.sh
