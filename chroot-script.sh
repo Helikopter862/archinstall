206896532033dbd1c357a82e4c583216d04dc47c
Helikopter862 <130805628+Helikopter862@users.noreply.github.com>
2023-08-11 17:46:22 +0200

    Update chroot-script.sh

--- chroot-script.sh
+++ chroot-script.sh
@@ -72,5 +72,5 @@ mkdir -p ~/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
 nvim ~/.config/bspwm/bspwmrc
 mkdir -p ~/.config/sxhkd/
-curl -L "https://raw.githubusercontent.com/Helikopter862/dotfiles/main/sxhkdrc" -o ~/.config/sxhkd/sxhkdrc
-curl -L "https://raw.githubusercontent.com/Helikopter862/dotfiles/main/.xinitrc -o /home/karol/.xinitrc
+curl -L "https://raw.githubusercontent.com/Helikopter862/dotfiles/main/sxhkdrc" -o .config/sxhkd/sxhkdrc
+curl -L "https://raw.githubusercontent.com/Helikopter862/dotfiles/main/.xinitrc" -o .xinitrc
