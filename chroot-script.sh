ea869d2f6b6c5140c91d6069730fa6e2d770e95d
Helikopter862 <130805628+Helikopter862@users.noreply.github.com>
2023-08-11 20:24:03 +0200

    Update chroot-script.sh

--- chroot-script.sh
+++ chroot-script.sh
@@ -73,7 +73,7 @@ sudo mkinitcpio -P
 
 mkdir -p ~/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
-mkdir -p /home/karol/.config/alacritty /home/karol/.config/polybar /home/karol/.config/rofi
+mkdir -p /home/karol/.config/alacritty /home/karol/.config/polybar /home/karol/.config/rofi /home/karol/.config/sxhkd
 curl -L "https://raw.githubusercontent.com/Helikopter862/dotfiles/main/sxhkdrc" -o /home/karol.config/sxhkd/sxhkdrc
 curl -L "https://raw.githubusercontent.com/Helikopter862/dotfiles/main/.config/alacritty/alacritty.yml" -o /home/karol/.config/alacritty/alacritty.yml
 curl -L "https://raw.githubusercontent.com/Helikopter862/dotfiles/main/.config/polybar/config.ini" -o /home/karol/.config/polybar/config.ini
