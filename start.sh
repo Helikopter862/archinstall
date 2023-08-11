48f63bb7e9b02f70a7fcd4a10bee4449fd9701b6
Helikopter862 <130805628+Helikopter862@users.noreply.github.com>
2023-08-11 16:03:58 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-mkfs.xfs -f /dev/nvme0n1p2 || exit && mount /dev/nvme0n1p2 /mnt && mkdir -p /mnt/boot /mnt/media/data
-mkfs.fat -F32 /dev/nvme0n1p1 || exit && mount /dev/nvme0n1p1 /mnt/boot
-mount /dev/sda1 /mnt/media/data || exit
+mkfs.xfs -f /dev/sda2 || exit && mount /dev/sda2 /mnt && mkdir -p /mnt/boot /mnt/media/data
+mkfs.fat -F32 /dev/sda1 || exit && mount /dev/sda1 /mnt/boot
 
 pacstrap /mnt base linux-zen linux-zen-headers linux-firmware || exit
 timedatectl set-ntp true
