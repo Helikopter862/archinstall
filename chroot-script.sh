15ffd60cd90d1e8338d42df44869ebeb0d8587a5
Helikopter862 <130805628+Helikopter862@users.noreply.github.com>
2023-08-11 17:18:10 +0200

    Update chroot-script.sh

--- chroot-script.sh
+++ chroot-script.sh
@@ -2,17 +2,15 @@
 
 ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 hwclock --systohc
-
 sed -i "/^#en_US.UTF-8/ cen_US.UTF-8 UTF-8" /etc/locale.gen
 locale-gen
-
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
 echo archlinux >> /etc/hostname
 
 pacman --noconfirm -Sy neovim base-devel git
 pacman --noconfirm -S networkmanager
 systemctl enable NetworkManager
-
 pacman --noconfirm -S intel-ucode
 
 bootctl install
@@ -27,16 +25,15 @@ echo "title  Arch Linux
 linux  /vmlinuz-linux-zen
 initrd  /intel-ucode.img
 initrd  /initramfs-linux-zen.img
-options  root=/dev/nvme0n1p2 rw 
+options  root=/dev/nvme0n1p2 rw
 options  mitigations=off nvidia-drm.modeset=1" > /boot/loader/entries/arch.conf
- 
- 
+
 echo "title  Arch Linux Fallback 
 linux  /vmlinuz-linux-zen
 initrd  /intel-ucode.img
 initrd  /initramfs-linux-zen-fallback.img
 options  root=/dev/nvme0n1p2 rw
-options  mitigations=off nvidia-drm.modeset=1" > /boot/loader/entries/arch-fallback.conf
+options. mitigations=off nvidia-drm.modeset=1" > /boot/loader/entries/arch-fallback.conf
 
 echo "[Trigger]
 Type = Package
@@ -52,25 +49,28 @@ if ! [ $rootpw ]
 then
   rootpw="root"
 fi
-if ! [ $username ]
-then
-  username="user"
-fi
+
 if ! [ $userpw ]
 then
   userpw="user"
 fi
 
 echo "root:$rootpw" | chpasswd
-useradd -mg wheel $username
-echo "$username:$userpw" | chpasswd
+useradd -mg wheel karol
+echo "karol:$userpw" | chpasswd
 sed -i "/^# %wheel ALL=(ALL:ALL) ALL/ c%wheel ALL=(ALL:ALL) ALL" /etc/sudoers
 
 sed -i "/^#Color/cColor" /etc/pacman.conf
 sed -i "/^#ParallelDownloads/cParallelDownloads = 5" /etc/pacman.conf
-sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-pacman -Syu
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sudo pacman -Syu
 
-pacman --noconfirm -S xorg xorg-xinit noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra pipewire lib32-pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack pulsemixer neofetch htop mpv bspwm sxhkd polybar alacritty rofi feh thunar unzip flameshot
-pacman --noconfirm --needed -S lib32-libglvnd lib32-nvidia-utils lib32-vulkan-icd-loader libglvnd nvidia-dkms nvidia-settings vulkan-icd-loader
-mkinitcpio -P
+pacman --noconfirm --needed -S xorg noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra pipewire lib32-pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack pulsemixer lib32-libglvnd lib32-nvidia-utils lib32-vulkan-icd-loader libglvnd nvidia-dkms nvidia-settings vulkan-icd-loader neofetch htop mpv bspwm sxhkd polybar alacritty rofi feh thunar unzip flameshot
+sudo mkinitcpio -P
+
+mkdir -p ~/.config/bspwm/
+cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+nvim ~/.config/bspwm/bspwmrc
+mkdir -p ~/.config/sxhkd/
+curl -L "https://raw.githubusercontent.com/Helikopter862/dotfiles/main/sxhkdrc" -o ~/.config/sxhkd/sxhkdrc
+curl -L "https://raw.githubusercontent.com/Helikopter862/dotfiles/main/.xinitrc -o /home/karol/.xinitrc
